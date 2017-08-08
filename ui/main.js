2402b2d2cec35944e497f1852cf77e415aeef43e
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-08 22:45:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,5 +9,5 @@ function moveRight(){
 }
 img.onclick =function(){
     //image.style.marginleft ="100px";
-    var interval = setinterval(moveRight,50);
+    var interval = setInterval(moveRight,50);
 };
