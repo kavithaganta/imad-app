fb25c3e03a81502aac903706ebb660647c5aaeca
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-08 18:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ var articleOne = {
                       This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
                   </p>`
 };
+function createTemplate(data){
+var  title = data.title;
+var heading = data.heading;
+var date = data.date;
+var content = data.content;
+
 var htmlTemplate =`
    <html>
 <head>
@@ -46,12 +52,13 @@ var htmlTemplate =`
       </body>
 </html>
 `;
-
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res) {
-   res.sendFile(path.join(__dirname,'ui','article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 app.get('/article-two',function (req,res) {
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
