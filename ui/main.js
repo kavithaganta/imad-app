a7f640cb219563819bec218c22b22de73df5d7dd
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-08 22:34:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "Mokshagna";
+var img = document.getElementById("madi");
+img.onclick = function()
+{
+    image.style.marginleft ="100px";
+};
