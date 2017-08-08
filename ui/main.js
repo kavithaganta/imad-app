4b76912b21eee717230af7eb34aa743dd8803eac
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-08 22:52:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "Mokshagna";
-var image = document.getElementById("madi");
+var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 1;
-    image.style.marginLeft = marginLeft + "px";
+    img.style.marginLeft = marginLeft + "px";
 }
 img.onClick =function(){
     //image.style.marginleft ="100px";
