8de0d1f5ea51ad6eb3f53aabcec2ea2ec1d2d631
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-08 23:05:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ var img = document.getElementById("madi");
     img.style.marginLeft = marginLeft + "px";
 }*/
 img.onClick =function(){
-   img.style.marginLeft ="100px";
+   img.style.marginLeft = " 100px ";
    // var interval = setInterval(moveRight(),50);
 };
