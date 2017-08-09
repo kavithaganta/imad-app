18c094901f694829d9bc088f8d49e77c10cb4866
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-09 16:32:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ img.onclick = function(){
 } ;`
 
 var button = document.getElementById("counter");
- var counter = 0;
+ //var counter = 0;
  button.onclick = function()
  {
      var request = new XMLHTTPRequest();
