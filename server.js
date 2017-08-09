78e11ae9ecf4867e8f1d1cd895d6799e9e112253
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-09 21:56:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.get('/counter', function (req, res){
 });
 var names=[];
 //gives name along with url
-app.get('/submit-name/:name',function(req,res){
-    var name =req.params.name;
+app.get('/submit-name',function(req,res){
+    var name =req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
 });
