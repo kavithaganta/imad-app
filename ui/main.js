6371c39696dce0f9ee7da27fa66dcd4837b68338
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-09 21:59:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,7 +47,7 @@ var button = document.getElementById("counter");
     //make the request
      var nameInput =document.getElementById("name");
      var name=nameInput.value;
-request.open('GET','http://gantakavitha.imad.hasura-app.io/submit-name?name',true);
+request.open('GET','http://gantakavitha.imad.hasura-app.io/submit-name?name' +name,true);
 request.send(null);
 };
 //submit name
