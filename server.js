9431dc4a30b29626c85350623f3e5692d8c6d749
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-08-21 22:39:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool =new pool(config);
-app.get('test-db',function(req,res){
+app.get('/test-db',function(req,res){
     pool.query('select *from test',function(req,res){
         if(err){
             res.error.status(500).send(err.toString());
