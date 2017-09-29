6ad89f563fc78a4a2131c9d5ccf897ac2c6ea9af
kavithaganta <30657608+kavithaganta@users.noreply.github.com>
2017-09-29 12:37:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,6 +125,9 @@ app.get('/articles/:articleName',function (req,res) {
    // var articleName = req.params.articleName;
 //    res.send(createTemplate(articles[articleName]));
 });
+app.get('/ui/index1.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index1.html'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
